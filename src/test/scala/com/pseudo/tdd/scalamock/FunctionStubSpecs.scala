9b5601f04ab6c9a987a87529b1111584802bd664
prayagupd <prayag.upd@gmail.com>
2018-09-28 20:15:29 -0700

    stubFunction with multiple params

--- src/test/scala/com/pseudo/tdd/scalamock/FunctionStubSpecs.scala
+++ src/test/scala/com/pseudo/tdd/scalamock/FunctionStubSpecs.scala
@@ -21,4 +21,47 @@ class FunctionStubSpecs extends FunSuite with MockFactory with Matchers {
 
     shippedNotifications shouldBe Seq("shipping item1", "shipping item2")
   }
+
+
+  import java.util.Base64
+
+  object Api {
+
+    def getActionAsBase64(fileBytesFn: (String, String, String) => Array[Byte],
+                          appName: String,
+                          taskType: String,
+                          taskName: String): String = {
+
+      val encoded = new String(Base64.getEncoder
+        .encode(fileBytesFn(appName, taskName, taskName)))
+        .replace("\n", "")
+        .replace("\r", "")
+
+      encoded
+    }
+  }
+
+  test("test a function") {
+
+    val mock = (_: String, _: String, _: String) => "prayagupd".getBytes()
+
+    val d = Api.getActionAsBase64(mock, "any app name", "taskName", "taskName")
+
+    assert(d == "cHJheWFndXBk")
+  }
+
+  def fn = new Function3[String, String, String, Array[Byte]] {
+    override def apply(v1: String, v2: String, v3: String) = Array.emptyByteArray
+  }
+
+  test("test a function II") {
+
+    val mock = stubFunction[String, String, String, Array[Byte]]
+    mock.when(*, *, *).returns("prayagupd".getBytes())
+
+    val d = Api.getActionAsBase64(mock, "any appName", "any taskType", "any taskName")
+
+    assert(d == "cHJheWFndXBk")
+  }
+
 }
